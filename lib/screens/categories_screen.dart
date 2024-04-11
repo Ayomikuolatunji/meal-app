8673aff7f097a7bc0aa097b74e768f6104021ea3
ayomikuolatunji <ayomikuolatunji@gmail.com>
2024-04-11 10:57:39 +0100

    What: meals UI building

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:net_ninja_course/data/dummy_data.dart';
+import 'package:net_ninja_course/screens/meals_screen.dart';
+import "package:net_ninja_course/models/category.dart";
 import 'package:net_ninja_course/widgets/category_widget.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) => MealsScreen(
+                  title: category.title,
+                  meals: dummyMeals
+                      .where((meal) => meal.categories.contains(category.id))
+                      .toList(),
+                )));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +34,10 @@ class CategoriesScreen extends StatelessWidget {
               mainAxisSpacing: 20),
           children: [
             for (final category in availableCategories)
-              CategoryGridItem(category: category)
+              CategoryGridItem(
+                category: category,
+                selectCategory: _selectCategory,
+              )
           ],
         ));
   }
--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:net_ninja_course/models/meal.dart';
 
-class MealsScreen extends StatelessWidget{
-
-  const MealsScreen({super.key});
+class MealsScreen extends StatelessWidget {
+  final String title;
+  final List<Meal> meals;
+  const MealsScreen({super.key, required this.meals, required this.title});
 
   @override
-
-  Widget build(BuildContext context){
-    return 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: meals.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "No meals",
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "Try selecting a new category",
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),
+                  )
+                ],
+              ),
+            )
+          : ListView.builder(
+              itemBuilder: ((ctx, index) => Text(meals[index].title)),
+              itemCount: meals.length,
+            ),
+    );
   }
 }
